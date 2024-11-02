525af0a09f3e972281a555b844d0161ed7fed9b7
Soumyashis Karmakar <soumyashisk@gmail.com>
2024-11-02 13:43:10 +0530

    Started to work on the detail page

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "gallery"
 urlpatterns = [
     path("", views.ArtworkListView.as_view(), name="home"),
+    path("<int:pk>", views.ArtworkDetailView.as_view(), name="detail"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,16 +1,13 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Artwork
 class ArtworkListView(ListView):
     model = Artwork
     template_name = "gallery/index.html"
     context_object_name = "artworks"
 
-
-# Create your views here.
-# def index(req):
-#     return render(
-#         req,
-#         "gallery/index.html",
-#         context={"item": map(lambda a: a * 200, range(1, 11))},
-#     )
+class ArtworkDetailView(DetailView):
+    model = Artwork
+    template_name = "gallery/detail.html"
+    context_object_name = "artwork"
