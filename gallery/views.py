9e6ae0f0c5f15323d8d84b8c4e46f163d5d064e8
Soumyashis Karmakar <soumyashisk@gmail.com>
2024-11-07 22:08:16 +0530

    Dislike feature

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.decorators.http import require_POST
@@ -14,16 +14,18 @@ class ArtworkListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+
         if self.request.user.is_authenticated:
             user = self.request.user
-            
-            liked_artwork_ids = set(Like.objects.filter(user=user).values_list('artwork_id', flat=True))
+
+            liked_artwork_ids = set(
+                Like.objects.filter(user=user).values_list("artwork_id", flat=True)
+            )
             print(liked_artwork_ids)
-        
-            for artwork in context['artworks']:
+
+            for artwork in context["artworks"]:
                 artwork.liked = artwork.id in liked_artwork_ids
-        
+
         return context
 
 
@@ -32,20 +34,32 @@ class ArtworkDetailView(DetailView):
     template_name = "gallery/detail.html"
     context_object_name = "artwork"
 
-# ISSUE: No dislike handling
+
 @require_POST
 def LikeView(req):
     form = LikeForm(req.POST or None)
-    if form.is_valid():
+    if form.is_valid() and req.user.is_authenticated:
         id = req.POST.get("id", None)
-        if req.user.is_authenticated and id is not None:
-            artwork = Artwork.objects.get(id=id)
-            like = Like(artwork=artwork, user=req.user)
-            like.save()
-            artwork.likes += 1
+        if id is not None:
+            try:
+                artwork = Artwork.objects.get(id=id)
+            except Artwork.DoesNotExist:
+                return HttpResponse("Artwork with that id is not found.")
+            like, created = Like.objects.get_or_create(user=req.user, artwork=artwork)
+
+            if created:
+                artwork.likes += 1
+                artwork.liked = True
+            else:
+                like.delete()
+                artwork.likes -= 1
+                artwork.liked = False
+
             artwork.save()
-    return render(
-        req,
-        "gallery/like.html",
-        context={"artwork": Artwork.objects.get(id=id)},
-    )
+            return render(
+                req,
+                "gallery/like.html",
+                context={"artwork": artwork},
+            )
+        else:
+            return HttpResponse(status=404)
