9e4580771b2bbdaab8ed822788928643e166c668
Soumyashis Karmakar <soumyashisk@gmail.com>
2024-11-07 21:00:12 +0530

    Shows if the current user has liked the post and no additional likes can be added if so

--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Artwork(models.Model):
-    # user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
     image = models.ImageField(upload_to="artworks")
@@ -17,8 +17,8 @@ class Artwork(models.Model):
     def get_absolute_url(self):
         return reverse("gallery:detail", kwargs={"pk": self.pk})
 
-# class Like(models.Model):
-    # artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # class Meta:
-    #     unique_together = ('artwork', 'user')
+class Like(models.Model):
+    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('artwork', 'user')
--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.decorators.http import require_POST
-from .models import Artwork
+from .models import Artwork, Like
 from .forms import LikeForm
 
 
@@ -12,20 +12,36 @@ class ArtworkListView(ListView):
     queryset = Artwork.objects.order_by("-created_at")
     context_object_name = "artworks"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            
+            liked_artwork_ids = set(Like.objects.filter(user=user).values_list('artwork_id', flat=True))
+            print(liked_artwork_ids)
+        
+            for artwork in context['artworks']:
+                artwork.liked = artwork.id in liked_artwork_ids
+        
+        return context
+
 
 class ArtworkDetailView(DetailView):
     model = Artwork
     template_name = "gallery/detail.html"
     context_object_name = "artwork"
 
-
+# ISSUE: No dislike handling
 @require_POST
-def Like(req):
+def LikeView(req):
     form = LikeForm(req.POST or None)
     if form.is_valid():
         id = req.POST.get("id", None)
         if req.user.is_authenticated and id is not None:
             artwork = Artwork.objects.get(id=id)
+            like = Like(artwork=artwork, user=req.user)
+            like.save()
             artwork.likes += 1
             artwork.save()
     return render(
