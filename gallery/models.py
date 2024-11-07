38b2d5679882b2d85c3bbf8482078f45d3b9b234
Soumyashis Karmakar <soumyashisk@gmail.com>
2024-11-07 22:17:51 +0530

    Now shows uploaded by username

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Artwork(models.Model):
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
     image = models.ImageField(upload_to="artworks")
