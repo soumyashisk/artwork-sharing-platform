1dac4832027afc5a2fb5349b3ee5f352c5438252
Soumyashis Karmakar <soumyashisk@gmail.com>
2024-11-13 23:05:07 +0530

    Added profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  image = models.ImageField(upload_to="profile_pics/")
+  artist_name = models.TextField(max_length=100)
+  bio = models.TextField(max_length=255, default="Hello, I am an artist.")
+  
+  def __str__(self):
+    return self.user.username
+
+  # def get_absolute_url(self):
+  #   return reverse("Profile_detail", kwargs={"pk": self.pk})
--- account/views.py
+++ account/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.detail import DetailView
+from .models import Profile
 
 
 def signup(req):
@@ -13,3 +15,12 @@ def signup(req):
     else:
         form = UserCreationForm()
     return render(req, "account/signup.html", context={"form": form})
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "account/profile.html"
+
+    def get_object(self):
+        username = self.kwargs.get("username")
+        return Profile.objects.get(username=username)
